5cb5b5a0891f429a879db8f3de97845ebd03a9c3
Oscar Dominguez <oscardc@gmx.com>
2015-11-28 11:27:11 +0100

    feat: (Test case 1) Matches records can be deleted.
    For this case a players table has been created, and a matches table that
    references these players.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
+import bleach
 import psycopg2
-
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     return psycopg2.connect("dbname=tournament")
 
+def execute(query, values):
+    # Connect to the database
+    conn = connect()
+
+    # Open a cursor to perform the operation
+    cursor = conn.cursor()
+
+    # Execute the command
+    if values is None:
+        cursor.execute(query)
+    else:
+        for value in values:
+            value = bleach.clean(value)
+        cursor.execute(query, values)
+
+    # Make the changes persistent
+    conn.commit()
+
+    # Close communication with the database
+    cursor.close()
+    conn.close()
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    query = "DELETE FROM matches;"
+    execute(query, None)
 
 def deletePlayers():
     """Remove all the player records from the database."""
@@ -25,14 +47,15 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
-
+    query = "INSERT INTO players (name) VALUES %s;"
+    execute(query, [name])
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -56,16 +79,16 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +96,3 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-
-
