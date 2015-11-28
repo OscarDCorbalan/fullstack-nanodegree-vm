eeb0c6d9875f4fc3bbd2b5974439d749b2c9e50a
Oscar Dominguez <oscardc@gmx.com>
2015-11-28 11:27:11 +0100

    feat: (Test case 1) Matches records can be deleted.
    For this case a players table has been created, and a matches table that
    references these players.

--- tournament/tournament.sql
+++ tournament/tournament.sql
@@ -3,7 +3,19 @@
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
 --
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+-- DROP existing tables before re-creating them.
+DROP TABLE IF EXISTS players CASCADE;
+DROP TABLE IF EXISTS matches;
 
+-- CREATE the tables we need;
+CREATE TABLE players (
+    id      serial PRIMARY KEY,
+    name    varchar(16) NOT NULL
+);
+
+CREATE TABLE matches (
+    player1     integer NOT NULL REFERENCES players(id),
+    player2     integer NOT NULL REFERENCES players(id),
+    winner      integer NOT NULL REFERENCES players(id)
+);
