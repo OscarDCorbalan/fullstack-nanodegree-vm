bc2429275a53353b7898c9866b1fd247973de639
Oscar Dominguez <oscardc@gmx.com>
2015-12-11 17:26:42 +0100

    feat: Support a real 8-players tournament
    See added function in tournament_test.py

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -20,22 +20,7 @@ CREATE TABLE matches (
     CHECK (winner != loser), -- winner and loser can't be the same player
     CHECK (winner > 0),
     PRIMARY KEY (winner, loser)
-);
-
---
--- http://www.postgresql.org/docs/9.2/static/plpgsql-trigger.html
-CREATE OR REPLACE FUNCTION check_inverse_match() RETURNS trigger AS $$
-BEGIN
-    IF EXISTS(SELECT * FROM matches WHERE winner=NEW.loser AND loser=NEW.winner)
-        THEN RAISE 'Key (winner, loser)=(%, %) already exists', NEW.winner, NEW.loser
-        USING ERRCODE = 'unique_violation';
-    END IF;
-    RETURN NEW;
-END;
-$$ language plpgsql;
-
-CREATE TRIGGER check_inverse_match BEFORE INSERT ON matches
-    FOR EACH ROW EXECUTE PROCEDURE check_inverse_match();
+); -- plus check the linked trigger at the bottom
 
 CREATE VIEW standings AS
     SELECT
@@ -51,3 +36,21 @@ CREATE VIEW standings AS
     ORDER BY
         wins DESC,
         games DESC;
+
+
+-- TRIGGERS
+
+-- Checks that a match between same players with different result does not exist
+-- http://www.postgresql.org/docs/9.2/static/plpgsql-trigger.html
+CREATE OR REPLACE FUNCTION check_inverse_match() RETURNS trigger AS $$
+BEGIN
+    IF EXISTS(SELECT * FROM matches WHERE winner=NEW.loser AND loser=NEW.winner)
+        THEN RAISE 'Key (winner, loser)=(%, %) already exists', NEW.loser, NEW.winner
+        USING ERRCODE = 'unique_violation';
+    END IF;
+    RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE TRIGGER check_inverse_match BEFORE INSERT ON matches
+    FOR EACH ROW EXECUTE PROCEDURE check_inverse_match();
