913966766223740650056668570f1c8c46455e75
Oscar Dominguez <oscardc@gmx.com>
2016-02-14 12:32:06 +0100

    refactor: Put queries in functions

--- vagrant/restaurants/problem-set-1/queries.py
+++ vagrant/restaurants/problem-set-1/queries.py
@@ -7,6 +7,35 @@ from random import randint
 import datetime
 import random
 
+# EXERCICE 2
+# 2.1. Query all of the puppies and return the results in ascending alphabetical order
+def getAllPuppies():
+    return session.query(Puppy.name).order_by(asc(Puppy.name)).all()
+
+# 2.2. Query all of the puppies that are less than 6 months old, youngest first
+def getYoungPuppies():
+    today = datetime.date.today()
+    before = today - datetime.timedelta(days = 180)
+    return session.query(Puppy).filter(Puppy.dateOfBirth.between(before, today))\
+            .order_by(desc(Puppy.dateOfBirth))\
+            .all()
+
+# 2.3. Query all puppies by ascending weight
+def getPuppiesByWeight():
+    today = datetime.date.today()
+    before = today - datetime.timedelta(days = 180)
+    return session.query(Puppy).filter(Puppy.dateOfBirth.between(before, today))\
+            .order_by(desc(Puppy.dateOfBirth))\
+            .all()
+
+# 2.4. Query all puppies grouped by the shelter in which they are staying
+def getShelters():
+    return session.query(Shelter).order_by(asc(Shelter.name)).all()
+
+def getPuppiesInShelter(shelter):
+    return session.query(Puppy).filter(Puppy.shelter == shelter)\
+            .order_by(Puppy.name)\
+            .all()
 
 engine = create_engine('sqlite:///puppyshelter.db')
 Base.metadata.bind = engine
@@ -15,58 +44,22 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-# EXERCISE 2 (queries)
-
-# 1. Query all of the puppies and return the results in ascending alphabetical order
-print "1. All puppies in alphabetical order"
-puppies = session.query(Puppy.name)\
-            .order_by(asc(Puppy.name))\
-            .all()
-for puppy in puppies:
+print "1. All puppies in alphabetical order:"
+for puppy in getAllPuppies():
     print puppy.name
 
-
-# 2. Query all of the puppies that are less than 6 months old organized by the youngest first
-print "2. Puppies less than 6 months old in age order"
-today = datetime.date.today()
-before = today - datetime.timedelta(days = 180)
-puppies = session.query(Puppy)\
-            .filter(Puppy.dateOfBirth.between(before, today))\
-            .order_by(desc(Puppy.dateOfBirth))\
-            .all()
-for puppy in puppies:
+print "2. Puppies less than 6 months old in age order:"
+for puppy in getYoungPuppies():
     print puppy.name, puppy.dateOfBirth
 
-
-# 3. Query all puppies by ascending weight
-print "3. All puppies in weight order"
-puppies = session.query(Puppy)\
-            .order_by(asc(Puppy.weight))\
-            .all()
-for puppy in puppies:
+print "3. All puppies in weight order:"
+for puppy in getPuppiesByWeight():
     print puppy.name, puppy.weight
 
 
-# 4. Query all puppies grouped by the shelter in which they are staying
-print "4. Puppies grouped by shelter"
-shelters = session.query(Shelter).order_by(asc(Shelter.name)).all()
-for shelter in shelters:
+#
+print "4. Puppies grouped by shelter:"
+for shelter in getShelters():
     print shelter.name
-    puppies = session.query(Puppy)\
-                .filter(Puppy.shelter == shelter)\
-                .order_by(Puppy.name)\
-                .all()
-    for puppy in puppies:
+    for puppy in getPuppiesInShelter(shelter):
         print " -", puppy.name
-
-
-# EXERCISE 3 (associations)
-print "5. All puppies in alphabetical order"
-puppies = session.query(Puppy)\
-            .order_by(asc(Puppy.name))\
-            .all()
-for puppy in puppies:
-    profile = session.query(PuppyProfile)\
-                .filter(PuppyProfile.puppy == puppy)\
-                .one()
-    print puppy.name, profile.picture
