e1216cc987b8c218bfbc038e90593ed3a6d91363
Oscar Dominguez <oscardc@gmx.com>
2016-02-14 13:10:10 +0100

    refactor: Put queries in functions

--- vagrant/restaurants/problem-set-1/queries.py
+++ vagrant/restaurants/problem-set-1/queries.py
@@ -1,16 +1,21 @@
 from sqlalchemy import create_engine, asc, desc
 from sqlalchemy.orm import sessionmaker
 
-from puppies import Base, Shelter, Puppy, PuppyProfile
+from puppies import *
 #from flask.ext.sqlalchemy import SQLAlchemy
 from random import randint
 import datetime
 import random
 
+engine = create_engine('sqlite:///puppyshelter.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 # EXERCICE 2
 # 2.1. Query all of the puppies and return the results in ascending alphabetical order
 def getAllPuppies():
-    return session.query(Puppy.name).order_by(asc(Puppy.name)).all()
+    return session.query(Puppy).order_by(asc(Puppy.name)).all()
 
 # 2.2. Query all of the puppies that are less than 6 months old, youngest first
 def getYoungPuppies():
@@ -37,11 +42,9 @@ def getPuppiesInShelter(shelter):
             .order_by(Puppy.name)\
             .all()
 
-engine = create_engine('sqlite:///puppyshelter.db')
-Base.metadata.bind = engine
-
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
+# EXERCISE 3 (associations)
+def getPuppyProfile(puppy):
+    return session.query(PuppyProfile).filter(PuppyProfile.puppy == puppy).one()
 
 
 print "1. All puppies in alphabetical order:"
@@ -56,10 +59,13 @@ print "3. All puppies in weight order:"
 for puppy in getPuppiesByWeight():
     print puppy.name, puppy.weight
 
-
-#
 print "4. Puppies grouped by shelter:"
 for shelter in getShelters():
     print shelter.name
     for puppy in getPuppiesInShelter(shelter):
         print " -", puppy.name
+
+print "5. Puppies with profiles"
+for puppy in getAllPuppies():
+    profile = getPuppyProfile(puppy)
+    print puppy.name, profile.picture
