48b22165d24a54dd023a478fd7baa366e474d9ac
Oscar Dominguez <oscardc@gmx.com>
2016-02-16 19:42:05 +0100

    feat: Basic get/post functionality

--- vagrant/restaurants/webserver.py
+++ vagrant/restaurants/webserver.py
@@ -1,5 +1,6 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-
+import cgi
+import sys
 
 class WebServerHandler(BaseHTTPRequestHandler):
 
@@ -8,14 +9,71 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            message = ""
-            message += "<html><body>Hello!</body></html>"
-            self.wfile.write(message)
-            print message
-            return
-        else:
-            self.send_error(404, 'File Not Found: %s' % self.path)
 
+            output = "<html><body>"
+            output += "Hello!"
+            output += """<form method='POST' enctype='multipart/form-data'
+                        action='/hello'>
+                        <h2>What would you like me to say?</h2>
+                        <input name='message' type='text'>
+                        <input value='Submit' type='submit'>
+                       </form>"""
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+            return
+
+        if self.path.endswith("/hola"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            output = "<html><body>"
+            output += "Hola!"
+            output += """<form method='POST' enctype='multipart/form-data'
+                        action='/hello'>
+                        <h2>What would you like me to say?</h2>
+                        <input name='message' type='text'>
+                        <input value='Submit' type='submit'>
+                       </form>"""
+            output += "</body></html>"
+            self.wfile.write(output)
+            print output
+            return
+      
+        self.send_error(404, 'File Not Found: %s' % self.path)
+
+    def do_POST(self):
+        try:
+            self.send_response(200)
+            self.end_headers()
+            print 'a'
+            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+            print 'b'
+            if ctype == 'multipart/form-data':
+                fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('message')
+            print messagecontent
+            output = ""
+            output +="<html><body>"
+            output +="<h2>Okay, how about this:<h2>"
+            output +="<h1>%s</h1>" % messagecontent[0]
+            output +="<form method='POST' enctype='multipart/form-data'\
+                        action='/hello'>\
+                        <h2>What would you like me to say?</h2>\
+                        <input name='message' type='text'>\
+                        <input value='Submit' type='submit'>\
+                        </form>"
+            output +="</body></html>"
+
+            self.wfile.write(output)
+            print output
+
+        except AttributeError as e:
+            print e
+        except:
+            print sys.exc_info()[0]
+            pass
 
 def main():
     try:
