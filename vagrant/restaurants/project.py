b6bb6c7edfe451a47535bd5827d94b2da512c7d8
Oscar Dominguez <oscardc@gmx.com>
2016-02-20 19:50:15 +0100

    feat: Show menu name in menu edit page

--- vagrant/restaurants/project.py
+++ vagrant/restaurants/project.py
@@ -34,9 +34,11 @@ def new_menu_item(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def edit_menu_item(restaurant_id, menu_id):
 	if request.method == 'GET':
+		menu = mnu_dao.get_menu(menu_id)
 		return render_template('editmenuitem.html',
 								restaurant_id = restaurant_id,
-								menu_id = menu_id)
+								menu_id = menu_id,
+								menu_name = menu.name)
 
 	# Else it's a POST
 	new_name = request.form['name']
