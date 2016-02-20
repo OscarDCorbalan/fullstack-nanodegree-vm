8348bbc3dec42d875f3744f635c3af7d34ef1afa
Oscar Dominguez <oscardc@gmx.com>
2016-02-20 12:34:54 +0100

    refactor: Externalize RestaurantsDAO to daos.py file

--- vagrant/restaurants/webserver.py
+++ vagrant/restaurants/webserver.py
@@ -3,55 +3,7 @@ import sys
 
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from database_setup import Base, Restaurant, MenuItem
-
-
-class RestaurantsDAO():
-
-    def __init__(self):
-        engine = create_engine('sqlite:///restaurantmenu.db')
-        Base.metadata.bind = engine
-        DBSession = sessionmaker(bind=engine)
-        self.session = DBSession()
-
-
-
-    def close(self):
-        #print "closing connection"
-        self.session.close()
-
-
-    def get_all_restaurants(self):
-        return self.session.query(Restaurant).all()   
-
-
-    def get_restaurant(self, rest_id):
-        return self.session.query(Restaurant).filter_by(id=rest_id).one()
-
-
-    def set_restaurant_name(self, rest_id, new_name):
-        restaurant = self.get_restaurant(rest_id)
-        if restaurant:
-            restaurant.name = new_name
-            self.session.add(restaurant)
-            self.session.commit()
-
-
-    def add_restaurant(self, new_name):
-        new_restaurant = Restaurant(name = new_name)
-        self.session.add(new_restaurant)
-        self.session.commit()
-
-
-    def delete_restaurant(self, rest_id):
-        restaurant = self.get_restaurant(rest_id)
-        if restaurant:
-            self.session.delete(restaurant)
-            self.session.commit()
-
+from daos import RestaurantsDAO
 
 
 class WebServerHandler(BaseHTTPRequestHandler):
