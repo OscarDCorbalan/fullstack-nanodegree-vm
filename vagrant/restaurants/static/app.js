11f9c9de6b86ca423808faa9823873455364d2fe
Oscar Dominguez <oscardc@gmx.com>
2016-02-27 12:34:02 +0100

    feat: Styling with Bootstrap

--- vagrant/restaurants/static/app.js
+++ vagrant/restaurants/static/app.js
@@ -6,7 +6,17 @@ window.onload = () => {
 		// Show a restaurant's action buttons only when hovering it
 		function toggleActions(){ $(this).find('a:not(.btn-default)').toggleClass('hidden'); };
 		$('#restaurant-list .btn-group').hover(toggleActions, toggleActions);
-	console.log('ad');
+	})();
+
+	(function showmenu_html(){
+		// Adjust all menu item boxes to have the same height
+		var maxHeight = 0;
+		const thumbs = $('.thumbnail');
+		thumbs.each(function(){
+			var h = $(this).height();
+			if(h > maxHeight) maxHeight = h;
+		});
+		thumbs.height(maxHeight);
 	})();
 	
 };
