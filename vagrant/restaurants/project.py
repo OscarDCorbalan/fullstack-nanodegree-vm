9c0e82b75c4d7804622857fa3f792357ca3944f6
Oscar Dominguez <oscardc@gmx.com>
2016-02-27 20:34:07 +0100

    fix: Pass restaurant_id to RestaurantDAO, where menu_id was passed

--- vagrant/restaurants/project.py
+++ vagrant/restaurants/project.py
@@ -143,7 +143,7 @@ def edit_menu_item(restaurant_id, menu_id):
 def delete_menu_item(restaurant_id, menu_id):
 	if request.method == 'GET':
 		menu = mnu_dao.get_menu(menu_id)
-		rst_name = rst_dao.get_restaurant(menu_id).name
+		rst_name = rst_dao.get_restaurant(restaurant_id).name
 		return render_template('deletemenuitem.html', menu = menu, restaurant_name = rst_name)
 
 	# Else it's a POST
