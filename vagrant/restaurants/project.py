1f30b353c7f4dcfa94975d6c3ae710ace456a044
Oscar Dominguez <oscardc@gmx.com>
2016-03-06 20:23:36 +0100

    feat: Add Facebook login through OAuth

--- vagrant/restaurants/project.py
+++ vagrant/restaurants/project.py
@@ -120,26 +120,94 @@ def allowed_file(filename):
 		filename.rsplit('.', 1)[1] in ALLOWED_FILES
 
 
-
-# Web routes
+# Login and logout routes
 
 # Login route
 @app.route('/login')
 def showLogin():
+	# Create anti-forgery state token
 	state = ''.join(random.choice(string.ascii_uppercase + string.digits) 
 		for x in xrange(32))
 	login_session['state'] = state
 	return render_template('login.html', STATE=state)
 
 
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+		return json_response(401, 'Invalid state paremeter.')
+
+    access_token = request.data
+    print "access token received %s " % access_token
+
+    app_id = json.loads(
+    	open('client_secrets_fb.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(
+        open('client_secrets_fb.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.5/me"
+    # strip expire tag from access token
+    token = result.split("&")[0]
+
+    url = 'https://graph.facebook.com/v2.5/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout, let's strip out the information before the equals sign in our token
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.5/me/picture?%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    user_id = usr_dao.get_user_id(login_session['email'])
+    if not user_id:
+		user_id = usr_dao.add_user(
+			login_session['username'], 
+			login_session['email'], 
+			login_session['picture'])
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("You are now logged in as %s" % login_session['username'], "success")
+    return output
+
+
 # Login with Google Connect
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+	# Validate state token
 	if request.args.get('state') != login_session['state']:
 		return json_response(401, 'Invalid state paremeter.')
 	code = request.data
 	try:
-		ouath_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		ouath_flow = flow_from_clientsecrets('client_secrets_gc.json', scope='')
 		ouath_flow.redirect_uri = 'postmessage'
 		credentials = ouath_flow.step2_exchange(code)
 	except FlowExchangeError:
@@ -184,6 +252,7 @@ def gconnect():
 	login_session['username'] = data['name']
 	login_session['picture'] = data['picture']
 	login_session['email'] = data['email']
+	login_session['provider'] = 'google'
 
 	# See if user exists, if not make a new one
 	user_id = usr_dao.get_user_id(data['email'])
@@ -205,7 +274,42 @@ def gconnect():
 	return output
 
 
-# Disconnect Google Connect, revoking user's token and ressetting its session
+# Disconnect based on provider
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['credentials']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('show_restaurants'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('show_restaurants'))
+
+
+# Disconnect Facebook, revoking user's token and ressetting its session
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    # The access token must me included to successfully logout
+    access_token = login_session['access_token']
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "you have been logged out"	
+
+
+# Disconnect Google, revoking user's token and ressetting its session
 @app.route('/gdisconnect')
 def gdisconnect():
 	# Abort if not connected
@@ -217,23 +321,15 @@ def gdisconnect():
 	# Execute HTTP GET to revoke current token
 	access_token = credentials.access_token
 	url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
-	print url
+
 	h = httplib2.Http()
 	result = h.request(url, 'GET')[0]
 
-	if result['status'] == '200':
-		# Reset session
-		del login_session['credentials']
-		del login_session['gplus_id']
-		del login_session['username']
-		del login_session['email']
-		del login_session['picture']
-		flash('Successfully disconnected', 'success')
-		return redirect(url_for('show_restaurants'))
-	else:
+	if result['status'] != '200':
 		flash("Failed to revoke user's token", 'error')
 		return redirect(url_for('show_restaurants'))
 
+# Web routes
 
 # Our index, shows a list of all the restaurants, plus restaurant create/edit/delete links
 @app.route('/')
@@ -359,11 +455,10 @@ def new_menu_item(restaurant_id):
 	
 	# Else it's a POST
 	new_name = request.form['name'].strip()
-	mnu_dao.add_menu_item(restaurant_id, new_name)
+	mnu_dao.add_menu_item(restaurant_id, new_name, restaurant.user_id)
 	flash('New menu item created', 'success')
 
 	return redirect(url_for('show_menu', restaurant_id = restaurant_id))
-
 
 
 # Form to edit an existing menu item
